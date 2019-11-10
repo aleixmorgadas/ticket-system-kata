ea819fdcb440384c36126f1f549b653fdb8cb723
Aleix <aleixmp@users.noreply.github.com>
2019-11-10 12:22:02 +0100

    Adding sample test

---
+++ src/main/java/com/github/aleixmp/ticketsystem/Hello.java
@@ -0,0 +1,4 @@
+package com.github.aleixmp.ticketsystem;
+
+public class Hello {
+}
---
+++ src/test/java/com/github/aleixmp/ticketsystem/HealthTest.java
@@ -0,0 +1,13 @@
+package com.github.aleixmp.ticketsystem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class HealthTest {
+
+    @Test
+    void alwaysPass() {
+        assertTrue(true);
+    }
+}
